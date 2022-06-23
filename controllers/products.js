d1691d51c0a960def0b1b0f81bbb8656dbef2469
Tony Kowalewski <tony@kowalewski.dev>
2022-06-23 17:02:43 +0200

    update getProducts (query) & mongoose

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,14 @@ const Product = require('../models/product');
 
 const getProducts = async (req, res) => {
   // console.log(req.query);
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false;
+  }
+
+  const products = await Product.find(queryObj);
   res.status(200).json({ items: products.length, products });
 };
 
