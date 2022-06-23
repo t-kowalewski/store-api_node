0ed7776053e4ee992b8c795d1495094e90a9b800
Tony Kowalewski <tony@kowalewski.dev>
2022-06-23 14:20:47 +0200

    update/fix populate.js

--- populate.js
+++ populate.js
@@ -7,21 +7,20 @@ const Product = require('./models/product');
 
 const jsonProducts = require('./products.json');
 
-const start = () => {
-  connectDB(process.env.MONGO_URI)
-    .then(() => {
-      Product.deleteMany();
-      console.log('deleted');
-    })
-    .then(() => {
-      Product.create(jsonProducts);
-      console.log('added');
-      process.exit(0); //terminate running node process
-    })
-    .catch((err) => {
-      console.log(err);
-      process.exit(1);
-    });
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    console.log('deleted');
+
+    await Product.create(jsonProducts);
+    console.log('added');
+
+    process.exit(0); //terminate running node process
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 start();
